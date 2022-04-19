8ed3fb3bb9bc40d0821420a302406823ac806d00
Rohan Kalyanpura <Rohan.kalyanpura@student.allenisd.org>
2022-04-19 09:33:08 -0500

    code

--- main.swift
+++ main.swift
@@ -1,1 +1,17 @@
+import Foundation
 import Crypto
+
+
+func makehash(string: String) -> String {
+    guard let stringData = string.data(using: .utf8) else {
+        fatalError("FAiled to obtain data from String.")
+    }                
+
+    let hash = Insecure.MD5.hash(data: stringData)
+    let hashData = Data(hash)
+
+    let hexadecimalHash = hashData.map { String(format: "%02hhx", $0) }.joined()
+    return hexadecimalHash
+}
+let x = makehash(string:"WLDME_FOREVER2005")
+print(x)
